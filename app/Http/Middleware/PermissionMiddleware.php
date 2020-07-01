71d7e620c4c2ee25b747559d58fd29eefe8e4893
mail <ismail15000000@gmail.com>
2020-07-02 00:08:03 +0700

    update permission

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -8,22 +8,30 @@ use Spatie\Permission\Exceptions\UnauthorizedException;
 
 class PermissionMiddleware
 {
-    public function handle($request, Closure $next, $permission)
+    public function handle($request, Closure $next, $permission, $guard)
     {
-        if (Auth::guard('internal')->guest()) {
-            throw UnauthorizedException::notLoggedIn();
-        }
 
         $permissions = is_array($permission)
             ? $permission
             : explode('|', $permission);
 
+        $user = Auth::guard($guard)->user();
+
+        if(!$user) 
+        {
+            return response()->json([
+                'message' => 'Anda tidak berhak mengakses jaringan ' . $guard
+            ], 403);
+        }
+
         foreach ($permissions as $permission) {
-            if (Auth::guard('internal')->user()->can($permission)) {
+            if ($user->can($permission)) {
                 return $next($request);
             }
         }
 
-        throw UnauthorizedException::forPermissions($permissions);
+        return response()->json([
+            'message' => 'Anda tidak memiki Hak Akses untuk aksi ini'
+        ], 403);
     }
 }
